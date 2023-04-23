b396324b0a452141bdda778b26768d5f26e814c9
Ibrahim Biliaminu <75140077+ibravura@users.noreply.github.com>
2023-04-22 21:43:20 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '2ec3d7d9ecac86ca3d26da44a4be44a205bdbed8',
+	    'sonar.login': 'eb5f64a92bfaaa32bf63f60efd37092f2c4a8655',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
